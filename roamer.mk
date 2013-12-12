4523fbde24ed24b208171ba01faccb555704905b
Motorhead1991 <motor-head-1991@hotmail.com>
2013-12-12 02:13:25 -0800

    Path fixes, permissions fixes and other nonsense

--- roamer.mk
+++ roamer.mk
@@ -1,34 +1,26 @@
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_AAPT_CONFIG := normal mdpi
-PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_AAPT_CONFIG += normal mdpi
+PRODUCT_AAPT_PREF_CONFIG += mdpi
 PRODUCT_LOCALES += mdpi
-
-PRODUCT_COPY_FILES  +=\
-    $(LOCAL_PATH)/config/vold.fstab:system/etc/vold.fstab \
-    $(LOCAL_PATH)/config/fw_4319_apsta.bin:system/etc/fw_4319_apsta.bin \
-    $(LOCAL_PATH)/config/fw_4319.bin:system/etc/fw_4319.bin\
-    $(LOCAL_PATH)/config/nv_4319.txt:system/etc/nv_4319.txt\
-    $(LOCAL_PATH)/config/test_4319.bin:system/etc/test_4319.bin
 
 # Permissions
 PRODUCT_COPY_FILES += \
-	frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-	frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-	frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-	frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    $(ANDROID_BUILD_TOP)/frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
 
 # Key layout ank keychar binaries
-PRODUCT_COPY_FILES +=\
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keyfiles/qwerty2.kcm.bin:system/usr/keychars/qwerty.bin \
     $(LOCAL_PATH)/keyfiles/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
     $(LOCAL_PATH)/keyfiles/qwerty.kl:system/usr/keylayout/qwerty.kl \
@@ -37,7 +29,7 @@ PRODUCT_COPY_FILES +=\
     $(LOCAL_PATH)/keyfiles/roamer_keypad.kl:system/usr/keylayout/roamer_keypad.kl
 
 # Publish that we support the live wallpaper feature.
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # Audio Parts
@@ -45,6 +37,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/AudioFilter.csv:system/etc/AudioFilter.csv \
     $(LOCAL_PATH)/config/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
     $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml
+
+PRODUCT_COPY_FILES  += \
+    $(LOCAL_PATH)/config/vold.fstab:system/etc/vold.fstab \
+    $(LOCAL_PATH)/config/fw_4319_apsta.bin:system/etc/fw_4319_apsta.bin \
+    $(LOCAL_PATH)/config/fw_4319.bin:system/etc/fw_4319.bin\
+    $(LOCAL_PATH)/config/nv_4319.txt:system/etc/nv_4319.txt\
+    $(LOCAL_PATH)/config/test_4319.bin:system/etc/test_4319.bin \
+    $(LOCAL_PATH)/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
 # Modules
 PRODUCT_COPY_FILES += \
